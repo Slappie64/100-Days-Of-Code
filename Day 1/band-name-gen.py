317a85218c23cecd474272f6aebc78a5bad03777
Luigi Marino <emails@luigi-marino.com>
2023-08-15 12:43:43 +0100

    new file:   Day 1/band-name-gen.py

---
+++ Day 1/band-name-gen.py
@@ -0,0 +1,10 @@
+welcome_message = "Welcome to the Band Name Generator!"
+
+print(welcome_message)
+print("-" * len(welcome_message))
+
+town_name = input("Enter the town you grew up in: \n")
+pet_name = input("Enter the name of your first pet: \n")
+
+print("\n")
+print(town_name + " " + pet_name)
